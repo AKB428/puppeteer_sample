a4ee2efcf579c9c0a83be4fdaa9f111f6af0af6c
AKB428 <gandara016+1@gmail.com>
2021-09-04 17:59:17 +0900

    add ignore

--- sample2-arg.js
+++ sample2-arg.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   // JSファイルの後の第一引数  [0]node [1]JSファイル名
   await page.goto(process.argv[2]);
-  await page.screenshot({ path: 'example.png' });
+  await page.screenshot({ path: 'example1.png' });
 
   await browser.close();
 })();
